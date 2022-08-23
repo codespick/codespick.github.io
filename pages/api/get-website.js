e6504058ec4842d0f1091ad8394b05843fad16f0
codespick <gzone2434@gmail.com>
2022-08-23 12:57:44 +0530

    make it better

--- pages/api/get-website.js
+++ pages/api/get-website.js
@@ -1,24 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 
-export default function getWebsite(req, res, next) {
-  if (req.secure) {
-    fs.readFile(
-      `website-search-page/${req.query.q?.toLowerCase()}.json`,
-      "utf-8",
-      (err, data) => {
-        if (err) {
-          res
-            .status(404)
-            .json({ error: `The search ${req.query.q} could not be found` });
-        } else {
-          res.status(200).send(JSON.parse(data));
-        }
-        // console.log(JSON.parse(data))
+export default function getWebsite(req, res) {
+  fs.readFile(
+    `website-search-page/${req.query.q?.toLowerCase()}.json`,
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        res
+          .status(404)
+          .json({ error: `The search ${req.query.q} could not be found` });
+      } else {
+        res.status(200).send(JSON.parse(data));
       }
-    );
-    next();
-  } else {
-    res.redirect("https://" + req.headers.host + req.url);
-  }
+      // console.log(JSON.parse(data))
+    }
+  );
 }
